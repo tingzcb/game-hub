6f9d5f393629f94c680c66b79f605c25c7720f9f
tingzcb <tingting.sun@cqumail.com>
2023-07-18 10:58:06 +1000

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 
-interface Game {
+export interface Game {
     id: number;
     name: String;
+    background_image: string;
   }
   
   interface FetchGamesResponse {
